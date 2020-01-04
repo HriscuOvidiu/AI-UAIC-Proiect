87f520e06a2b321380cb2b53695aa3dfffb4ea35
Ovidiu Hriscu <hriscuo@gmail.com>
2020-01-04 18:07:45 +0200

    Added basic index.html interaction

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,27 @@
+function addInitialState(tag) {
+    const elements = $(`.choice.${tag}`);
+    elements.first().addClass('selected');
+    elements.click(function(e){
+        e.preventDefault();
+        
+        elements.removeClass('selected');       
+        $(this).addClass('selected');
+
+        const text = $(this).text();
+        info[tag] = text;
+        console.log(info);
+    });
+};
+
 function runGame(){
     window.location.href='/game';
 };
+
+const info = {};
+const tags = ['rule', 'game-type', 'ai-type'];
+
+$(document).ready(() => {
+    tags.forEach((t) => {
+        addInitialState(t);
+    });
+});
