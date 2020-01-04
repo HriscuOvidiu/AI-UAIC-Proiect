c799c13a317ef56f63496440526e0413cbd74a03
ilieandrei98 <ilandrei98@gmail.com>
2020-01-04 18:19:21 +0200

    Prepared for dynamically displaying chess pieces

--- app.py
+++ app.py
@@ -8,27 +8,41 @@ CORS(app)
 # Front-end routes
 ##
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/game')
 def game():
-    state = {};
-    return render_template('game.html', message='pula')
+    state = [["" for i in range(8)] for i in range(8)]
+    for row in state:
+        for index, cell in enumerate(row):
+            if cell != "":
+                row[index] = f'assets/{cell}.png'
+            else:
+                row[index] = 'assets/blank.png'
+
+    print(state)
+
+    return render_template('game.html', initial_state=state)
 
 ##
 # API
 ##
 
-@app.route('/api/availableMoves', methods=['GET','POST'])
+
+@app.route('/api/availableMoves', methods=['GET', 'POST'])
 def availableMoves():
     print(request.get_json())
-    return "<h1>Hello</h1>";
+    return "<h1>Hello</h1>"
+
 
 @app.route('/api/move', methods=['POST'])
 def move():
     pass
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
