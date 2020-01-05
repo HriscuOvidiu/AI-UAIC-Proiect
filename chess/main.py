f626cc41c22680fb244eefbfcc88aaf0854b2114
Andrei Petrescu <andrei.p2998@yahoo.com>
2020-01-05 16:41:50 +0200

    ..

--- chess/main.py
+++ chess/main.py
@@ -1,5 +1,6 @@
 from chess.game.ChessGame import ChessGame
 from chess.game.Config import Config
+import json
 
 
 game_dict = {
@@ -8,21 +9,15 @@ game_dict = {
                 2: "./static/configs/endgame_chess_cfg.json"
             }
 
-players_dict = {0: 
-                    {
-                        0: "minimax", 
-                        1: "alpha_beta_prunning", 
-                        2: "reinforcement_learning"}, 
-                1: {
-                        
-                }}
+ai_modes =  {
+                -1: None,
+                0: "minimax",
+                1: "alpha_beta_prunning",
+                2: "reinforcement_learning"
+            }
 
-strategy_dict = {0: "minimax"}
-
-
-def setup(game, players, strategy):
-
-
+def setup(setup_json):
+    pass
 
 def get_board():
     chess_config = Config("./static/configs/standard_chess_cfg.json")
