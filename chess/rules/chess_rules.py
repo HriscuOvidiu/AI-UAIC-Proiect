f0b6a00eefa7c7ff6447248a4d3380867359d642
constantinescuv <constantinescuvlad12@gmail.com>
2020-01-06 15:49:14 +0200

    chess_rules

--- chess/rules/chess_rules.py
+++ chess/rules/chess_rules.py
@@ -55,21 +55,26 @@ def standard_rook(start_cell: Cell, chess: ChessGame):
     for i in range(chess._configuration.get_board_lines()):
         if i == start_cell.position.line:
             continue
-        if chess.current_state.board[i][start_cell.position.column]._is_empty() == False:
-            if different_color(start_cell, chess.current_state.board[i][start_cell.position.column]):
-                add_move_by_location(valid_moves, chess, i, start_cell.position.column)
-                continue
-        else:
+        if chess.current_state.board[i][start_cell.position.column]._is_empty():
             add_move_by_location(valid_moves, chess, i, start_cell.position.column)
-    for j in range(chess._configuration.get_board_columns()):
+            continue
+        elif different_color(start_cell, chess.current_state.board[i][start_cell.position.column]):
+            add_move_by_location(valid_moves, chess, i, start_cell.position.column)
+            break
+        else:
+            break
+
+    for j in range(chess._configuration.get_board_lines()):
         if j == start_cell.position.column:
             continue
-        if chess.current_state.board[start_cell.position.line][j]._is_empty() == False:
-            if different_color(start_cell, chess.current_state.board[start_cell.position.line][j]):
-                add_move_by_location(valid_moves, chess, start_cell.position.line, j)
-                continue
-        else:
+        if chess.current_state.board[start_cell.position.line][j]._is_empty():
             add_move_by_location(valid_moves, chess, start_cell.position.line, j)
+            continue
+        elif different_color(start_cell, chess.current_state.board[start_cell.position.line][j]):
+            add_move_by_location(valid_moves, chess, start_cell.position.line, j)
+            break
+        else:
+            break
     return valid_moves
 
 def can_move_column(cell, chess, direction):
@@ -95,7 +100,7 @@ def get_king_moves(start_cell, chess):
 def get_king_captures(start_cell, chess):
     king_captures = []
     king_captures.extend(pawn_can_capture(start_cell, chess, 1))
-    #king_captures.extend(pawn_can_capture(start_cell, chess, -1))
+    king_captures.extend(pawn_can_capture(start_cell, chess, -1))
     return king_captures
     
 
@@ -113,11 +118,40 @@ def standard_bishop(start_cell: Cell, chess: ChessGame):
     for direction in directions:
         i = start_cell.position.line
         j = start_cell.position.column
-        for counter in range(0,8):
+        blocked = 0
+        for counter in range(0, chess._configuration.get_board_lines() + 1):
             i += direction[0]
             j += direction[1]
-            if chess.current_state.board[i][j]._is_empty() or different_color(start_cell, chess.current_state.board[i][j]):
+            if 0 <= i <= 7 and 0 <= j <= 7 and blocked == 0:
+                if chess.current_state.board[i][j]._is_empty():
+                    add_move_by_location(valid_moves, chess, i, j)
+                elif different_color(start_cell, chess.current_state.board[i][j]):
+                    add_move_by_location(valid_moves, chess, i, j)
+                else:
+                    blocked = 1
+
+    return valid_moves
+
+def standard_queen(start_cell: Cell, chess: ChessGame):
+    valid_moves = []
+    valid_moves.extend(standard_bishop(start_cell, chess))
+    valid_moves.extend(standard_rook(start_cell, chess))
+    return valid_moves
+
+def standard_knight(start_cell: Cell, chess: ChessGame):
+    valid_moves = []
+    directions = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]
+    for direction in directions:
+        i = start_cell.position.line
+        j = start_cell.position.column
+        i += direction[0]
+        j += direction[1]
+        if 0 <= i <= 7 and 0 <= j <= 7:
+            if chess.current_state.board[i][j]._is_empty():
                 add_move_by_location(valid_moves, chess, i, j)
+            elif different_color(start_cell, chess.current_state.board[i][j]):
+                add_move_by_location(valid_moves, chess, i, j)
+            
 
     return valid_moves
 
@@ -133,5 +167,7 @@ def standard_game_over(board):
 rules_func_dict = { "standard_pawn": standard_pawn,
                     "standard_rook": standard_rook,
                     "standard_king": standard_king,
-                    "standard_bishop": standard_bishop}
+                    "standard_bishop": standard_bishop,
+                    "standard_queen": standard_queen,
+                    "standard_knight": standard_knight}
 
