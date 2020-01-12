364cfe5daa4cc66e5047e5f1f4142f3f74d86182
Alin Ciocoveanu <33698707+alinciocoveanu@users.noreply.github.com>
2020-01-12 10:40:18 +0200

    Update ChessGame.py

--- chess/game/ChessGame.py
+++ chess/game/ChessGame.py
@@ -94,7 +94,7 @@ class ChessGame:
         end_cell.chess_piece = chess_piece_to_move
         start_cell.chess_piece = None
 
-    def move(self, start_line, start_column, end_line, end_column, promotion_piece_type='queen'):
+    def move(self, start_line, start_column, end_line, end_column, no_log=False, promotion_piece_type='queen'):
         # If PROMOTION
         if self.is_current_player_white():
             if end_line == 0 and self.current_state.board[start_line][start_column].chess_piece.name == 'Pawn':
@@ -112,7 +112,8 @@ class ChessGame:
         # If not the same position is selected
         if not (start_line == end_line and start_column == end_column):
             self.move_piece(start_line, start_column, end_line, end_column)
-            self.add_log(start_line, start_column, end_line, end_column, 'White' if self.current_state.is_current_player_white() else 'Black')
+            if not no_log:
+                self.add_log(start_line, start_column, end_line, end_column, 'White' if self.current_state.is_current_player_white() else 'Black')
             self.change_current_player()
 
     # TODO: Check
@@ -125,8 +126,6 @@ class ChessGame:
 
         pawn_cell.chess_piece = new_chess_piece
 
-        print(self.current_state.board[pawn_line][pawn_column])
-
     def has_finished(self):
         return self.configuration.get_end_condition()(self.current_state.current_player.color, self)
 
@@ -134,7 +133,10 @@ class ChessGame:
         return self.current_state.get_rendered_board()
 
     def get_next_moves(self):
+        from chess.main import get_valid_positions_check
+        from copy import deepcopy
         next_moves = []
+        temp_state = deepcopy(self.current_state)
         for line in self.current_state.board:
             for cell in line:
                 piece = cell.chess_piece if not cell.is_empty() else ''
@@ -142,55 +144,49 @@ class ChessGame:
                     piece_moves = piece.get_valid_moves(cell, self)
 
                     # TODO: Check if OK
-                    from chess.main import get_valid_positions_check
-                    from copy import deepcopy
                     piece_moves = get_valid_positions_check(deepcopy(self), cell.position.line, cell.position.column, piece_moves)
                     #
 
                     for position in piece_moves:
-                        new_board = deepcopy(self.current_state.board)
-                        start_cell = new_board[cell.position.line][cell.position.column]
-                        end_cell = new_board[position.line][position.column]
-                        end_cell.chess_piece = start_cell.chess_piece
-                        start_cell.chess_piece = None
-                        state = ChessState(self._black, new_board) if self.is_current_player_white() \
-                            else ChessState(self._white, new_board)
-                        next_moves.append((state, start_cell, end_cell))
+                        self.move(cell.position.line, cell.position.column, position.line, position.column, no_log=True)
+                        state = self.current_state
+                        next_moves.append((state, cell, self.current_state.board[position.line][position.column]))
+                        self.current_state = deepcopy(temp_state)
         return next_moves
 
     def minimax(self, state, depth, maximizing):
         if not depth or self.has_finished() == 2:
-            return state, -state.get_eval()
+            return state, state.get_eval()
         max_eval = -float("inf")
         max_state = None
         for possible_move in self.get_next_moves():
-            next_move, move_eval = self.minimax(possible_move, depth - 1, -maximizing)
+            next_move, move_eval = -self.minimax(possible_move, depth - 1, -maximizing)[1]
             if move_eval > max_eval:
                 max_eval = move_eval
                 max_state = possible_move
-        return max_state, -max_eval
+        return max_state, max_eval
 
     def minimax_pruning(self, state, depth, alpha, beta, maximizing):
         if not depth or self.has_finished() == 2:
-            return state, -state[0].get_eval(maximizing)
+            return state, state[0].get_eval(maximizing)
         max_eval = -float("inf")
         max_state = None
         for possible_move in self.get_next_moves():
-            move_eval = self.minimax_pruning(possible_move, depth - 1, -beta, -alpha, -maximizing)[1]
-            alpha = max(alpha, max_eval)
+            move_eval = -self.minimax_pruning(possible_move, depth - 1, -beta, -alpha, -maximizing)[1]
             if move_eval > max_eval:
                 max_eval = move_eval
                 max_state = possible_move
+            alpha = max(alpha, max_eval)
             if alpha >= beta:
                 break
-        return max_state, -max_eval
+        return max_state, max_eval
 
     def minimax_root(self, depth):
         next_move, score = self.minimax_pruning((self.current_state, None, None), depth, -float("inf"), float("inf"), 1)
+        # self.current_state = self.minimax(self.current_state, depth, 1)
         if self.has_finished() == 2:
             return -1
         self.current_state = next_move[0]
         self.add_log(next_move[1].position.line, next_move[1].position.column, next_move[2].position.line,
                      next_move[2].position.column, 'Black' if self.current_state.is_current_player_white() else 'White')
         print(f'{self.current_state.current_player}:{score}')
-        # self.current_state = self.minimax(self.current_state, depth)
